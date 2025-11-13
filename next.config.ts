11c29b8527a15c6dfe04dab1265db03ffc4d3328
bae-giyoung <giyoung1012@gmail.com>
2025-11-13 22:39:27 +0900

    next.config.ts deprecated 설정 제거

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
   async rewrites() {
     return [
       { source: '/java/:path*', destination: 'http://10.125.121.211:8080/:path*' },
