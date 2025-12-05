249eaf09ac9fa809ea8cf06cb3a6636f5e0d1358
bae-giyoung <giyoung1012@gmail.com>
2025-12-05 16:36:03 +0900

    fix: 헤더 및 푸터의 웹 시연 동영상 링크 변경

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -7,7 +7,7 @@ export default async function Footer () {
         <ul className="flex gap-4 md:gap-7 text-xs md:text-sm font-bold">
           <Link href="/privacy"><li>개인정보 처리 방침</li></Link>
           <Link href="/terms"><li>이용 약관</li></Link>
-          <Link href="https://www.awesomescreenshot.com/video/45335614?key=22b6bf02e01734f2ad832fc28d025077" target="_blank"><li>대시보드 시연 동영상</li></Link>
+          <Link href="https://www.awesomescreenshot.com/video/46379582?key=841a26872d250d5c3c5fcddca08a67d5" target="_blank"><li>대시보드 시연 동영상</li></Link>
         </ul>
         <div className="flex justify-between items-center">
           <div className="flex flex-col justify-center items-center gap-2.5 font-medium text-lg lg:text-xl tracking-tighter">MulAlim Lab 2025</div>
--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav () {
       <ul className="flex items-center justify-center gap-10 text-xl font-bold">
           <li><Link href={"/explain"}><span>대시 보드</span></Link></li>
           {/* <li><Link href={"/explainold"}><span>대시 보드 Old</span></Link></li> */}
-          <li><Link href={"https://www.awesomescreenshot.com/video/45335614?key=22b6bf02e01734f2ad832fc28d025077"} target="_blank"><span>시연 동영상</span></Link></li>
+          <li><Link href={"https://www.awesomescreenshot.com/video/46379582?key=841a26872d250d5c3c5fcddca08a67d5"} target="_blank"><span>시연 동영상</span></Link></li>
       </ul>
     </nav>
   );
